43f151a719f30bb1a36aff7acfad1204a156a6ab
fredslbn <fh.silaban@gmail.com>
2023-08-16 15:37:14 +0700

    hggf

--- .circleci/miatoll.sh
+++ .circleci/miatoll.sh
@@ -178,11 +178,11 @@ function exports() {
                export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
                export LD_LIBRARY_PATH="${KERNEL_DIR}/clang/lib:$LD_LIBRARY_PATH"
         
-        if [ -d ${KERNEL_DIR}/gcc64 ];
+        elif [ -d ${KERNEL_DIR}/gcc64 ];
            then
                export KBUILD_COMPILER_STRING=$("$KERNEL_DIR/gcc64"/bin/aarch64-elf-gcc --version | head -n 1)       
         
-        if [ -d ${KERNEL_DIR}/cosmic ];
+        elif [ -d ${KERNEL_DIR}/cosmic ];
            then
                export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/cosmic/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')        
         
